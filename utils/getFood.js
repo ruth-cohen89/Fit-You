372c7c7ad9dad708ba56b6a142ccebb3894fe961
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-04-18 21:22:15 +0300

    ,

--- utils/getFood.js
+++ utils/getFood.js
@@ -20,47 +20,36 @@ dotenv.config({ path: './config.env' });
 //   })
 //   .then(() => console.log('Connected to DB 🙂'));
 
-const requestFood = catchAsync(async (ingr) => {
-  console.log(ingr)
-  const apiUr = `https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.EDAMAM_APPID}&app_key=${process.env.EDAMAM_APPKEY}&ingr=chocolate&nutrition-type=cooking&category=generic-foods`;
+const requestFood = catchAsync(async () => {
   const apiUrl =
     'https://api.edamam.com/api/food-database/v2/parser?app_id=df5d4e31&app_key=28e85d3b17176dbda832b17f8c436c05&ingr=chocolate&nutrition-type=cooking&category=generic-foods';
-  console.log('b')
-  const response = await fetch(apiUrl);
-  console.log(response);
-  const data = await response.json();
-
-  return data;
 
   //const firstData = data.parsed[0];
   //console.log(data.parsed[0]);
 });
-requestFood();
-const createNewFood = catchAsync(async (data) => {
-  const newFood = Food.create({ id: data.foodId });
-  newFood.name = data.label;
-  newFood.foodId = data.foodId;
-  newFood.calories = data.nutrients.ENERC_KCAL;
-  newFood.protein = data.nutrients.PROCNT;
-  newFood.fat = data.nutrients.FAT;
-  newFood.carbs = data.nutrients.CHOCDF;
-  newFood.fiber = data.nutrients.FIBTG;
-  newFood.image = data.image;
-  console.log(newFood);
+
+const createNewFood = catchAsync(async (ingr) => {
+  const apiUri = `https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.EDAMAM_APPID}&app_key=${process.env.EDAMAM_APPKEY}&ingr=${ingr}&nutrition-type=cooking&category=generic-foods`;
+  const response = await fetch(apiUri);
+  const data = await response.json();
+  console.log(data, 'ma');
+  const newFood = await Food.create({
+    id: data.foodId,
+    name: data.label,
+    foodId: data.foodId,
+    calories: data.nutrients.ENERC_KCAL,
+    protein: data.nutrients.PROCNT,
+    fat: data.nutrients.FAT,
+    carbs: data.nutrients.CHOCDF,
+    fiber: data.nutrients.FIBTG,
+    image: data.image,
+  });
+  console.log(newFood, 'new food here');
 });
 
 const importFood = async () => {
-  requestFood();
   try {
-    
-    const basicProducts = [
-      'Chocolate',
-      'milk',
-      'cucumber',
-      'tomato',
-      'pepper',
-
-    ];
+    const basicProducts = ['Chocolate', 'milk', 'cucumber', 'tomato', 'pepper'];
     // 'orange',
     // 'salmon',
     // 'fish',
@@ -73,17 +62,16 @@ const importFood = async () => {
     // 'cheese',
     // 'bamba',
     // 'candy',
-    let data = '';
+    // let data = '';
     // eslint-disable-next-line no-plusplus
-    //for (let i = 0; i < basicProducts.length; i++) {
-      data = requestFood(basicProducts[0]);
-      //createNewFood(data.firstData.food);
-    //}
-    console.log('Food successfuly loaded');
+    for (let i = 0; i < basicProducts.length; i++) {
+      createNewFood(basicProducts[i]);
+    }
   } catch (err) {
     console.log(err);
   }
-  process.exit();
+  console.log('Food successfuly loaded');
+  //process.exit();
 };
 
 const deleteFood = async () => {
@@ -95,7 +83,8 @@ const deleteFood = async () => {
   }
   process.exit();
 };
-requestFood();
+importFood();
+
 //if (process.argv[2] === '--import') {
 //   requestFood();
 //   importFood();
