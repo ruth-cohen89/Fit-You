d69e01992926ce56970ea0a3b991c038b2c810ef
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-04-19 00:16:41 +0300

    l

--- models/foodModel.js
+++ models/foodModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const slugify = require('slugify');
 
-const mealSchema = new mongoose.Schema({
+const foodSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name of food.'],
@@ -11,19 +11,24 @@ const mealSchema = new mongoose.Schema({
   foodId: String,
   label: String,
   nutrients: {
-    calories: Number,
-    protein: Number,
+    calories: {
+      type: Number,
+      required: [true, 'How many calories are in this food?'],
+    },
+    protein: {
+      type: Number,
+      required: [true, 'How much protein is in this food?'],
+    },
     fat: Number,
     carbs: Number,
     fiber: Number,
   },
-  slug: String,
+  image: String,
 });
 
 // mealSchema.pre('save', function (next) {
 //   this.slug = slugify(this.name, { lower: true });
 //   next();
 // });
-
-// const Meal = mongoose.model('Meal', mealSchema);
-// module.exports = Meal;
+const Food = mongoose.model('Food', foodSchema);
+module.exports = Food;
