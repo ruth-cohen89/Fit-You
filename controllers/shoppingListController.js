5d810c9099bdf2b91bb2944d467a638a179408a0
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-05-11 19:36:14 +0300

    shoppingList completed

--- controllers/shoppingListController.js
+++ controllers/shoppingListController.js
@@ -3,11 +3,6 @@ const ShoppingList = require('../models/shoppingListModel');
 //const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getMyShoppingList = (req, res, next) => {
-  req.params.id = req.user.id;
-  next();
-};
-
 exports.createShoppingList = factory.createOne(ShoppingList);
 exports.getAllShoppingLists = factory.getAll(ShoppingList);
 exports.getShoppingList = factory.getOne(ShoppingList);
