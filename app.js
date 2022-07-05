250f0796be528bd1dbc6bf94993d925f2811ac60
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-05 17:24:15 +0300

    more

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour!',
+// });
 
-app.use('/api', limiter);
+//app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
