fabec73a9ebe89bb7924f3584cb2bf16e7d099dc
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-12 10:58:57 +0300

    cor

--- app.js
+++ app.js
@@ -32,20 +32,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Access-Control-Allow-Origin *
 app.use(cors());
-// app.options('*', cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-origin-Opener-Policy','same-origin');
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200)
-  } else {
-    next()
-  }
-});
+app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
