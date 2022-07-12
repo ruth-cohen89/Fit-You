5f6b883a5df9d5e6b2766c8592394128b118e054
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-07-12 11:12:16 +0300

    c

--- app.js
+++ app.js
@@ -69,18 +69,6 @@ app.use(
 
 app.use(compression());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
-  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-origin-Opener-Policy','same-origin');
-
   req.requestTime = new Date().toISOString();
   next();
 });
